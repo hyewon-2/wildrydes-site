4585607e8a16e3aa03082b3ebc810affacfc4f16
hyewon-2 <shw504@gmail.com>
2026-04-14 15:19:43 +0900

    v3

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-2_NmiX3YJjO', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2kjq2cor6sdk7r0g7eic21u2rn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '71mdmvl52bs9miaqie4ci7196g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
